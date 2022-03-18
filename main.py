e6c16113706e577c2beec1c55e938524bcf74746
Lakota Rose <itsrosebro@gmail.com>
2022-03-18 14:50:18 -0500

    Corrected main.py file, deleted 'print' calls.

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ alarm_one = Alarm()
     
 
 alarm_one.set_time()
-print(alarm_one.set_time)
-
 alarm_one.set_up_alarm()
-print(alarm_one.set_up_alarm)
-
 alarm_one.turn_alarm_on_or_off()
-print(alarm_one.turn_alarm_on_or_off)
 
